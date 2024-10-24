0756abe93e95957facbddf77031b9de408d3a62e
ewyser <manuwyser@gmail.com>
2024-10-24 07:43:49 +0200

    rework + enhancement

--- src/init/program/workflows/elastoplast/plast/RetMap/J2.jl
+++ src/init/program/workflows/elastoplast/plast/RetMap/J2.jl
@@ -1,4 +1,4 @@
-@views function J2Param(σ0,χ,nstr)
+@views function getJ2(σ0,χ,nstr)
     if nstr == 3
         P  = (σ0[1]+σ0[2])/2.0
         ξ  = σ0.-[P,P,0.0]
@@ -16,51 +16,50 @@
     end
     return P,q,n,ξn
 end
-@views function J2Yield(ξn,κ)
+@views function hasyield(σ,κ; χ::Real=3.0/2.0)
+    nstr     = length(σ)
+    P,q,n,ξn = getJ2(σ,χ,nstr)
     return f = ξn-κ
 end
-@views @kernel inbounds = true function J2!(mpD,ϵIIp,cmParam,instr) # Borja (1990); De Souza Neto (2008)
+@views @kernel inbounds = true function J2!(mpD,ϵIIp,cmParam,instr; ftol::Real= 1e-9,ηmax::Int= 20) # Borja (1990); De Souza Neto (2008)
     p = @index(Global)
     if p≤mpD.nmp 
-        ftol,ηtol,ηmax = 1e-9,1e4,20
-        Hp,χ = 0.35*cmParam.Hp,3.0/2.0
-        # create an alias
-        if instr[:fwrk] == :finite
-            σ,nstr = mpD.τ,size(mpD.τ,1)
-        elseif instr[:fwrk] == :infinitesimal
-            σ,nstr = mpD.σ,size(mpD.σ,1)
+        Hp = 0.35*cmParam.Hp
+        # create aliases
+        if instr[:fwrk] == "finite"
+            σ = mpD.τᵢ
+        elseif instr[:fwrk] == "infinitesimal"
+            σ = mpD.σᵢ
         end
         if first(instr[:nonloc])
             ϵII0 = mpD.ϵpII[:,2]
         else
             ϵII0 = mpD.ϵpII[:,1]
         end
-        P,q,n,ξn = J2Param(σ[:,p],χ,nstr)
-        κ        = 2.5*mpD.c0[p]+cmParam.Hp*ϵII0
-        if κ <= mpD.cr[p] κ = mpD.cr[p] end
-        f        = J2Yield(ξn,κ)
+        # calculate yield function
+        κ = max(mpD.cr[p],2.5*mpD.c0[p]+cmParam.Hp*ϵpII[p,1])
+        f = hasyield(σ[:,p],κ)
+        # return mapping using CPA (non-quadratic convergence)
         if f>0.0 
             γ0,σ0,ηit = copy(mpD.ϵpII[p]),copy(σ[:,p]),1
             while abs(f)>ftol && ηit<ηmax
-                ∂f∂σ     = n
-                Δλ       = f/(∂f∂σ'*cmParam.Del*∂f∂σ)
-                Δσ       = (Δλ*cmParam.Del*∂f∂σ)        
-                σ0     .-= Δσ 
-                γ0      += Δλ
-                P,q,n,ξn = J2Param(σ0,χ,nstr)
-                κ        = 2.5*mpD.c0[p]+cmParam.Hp*ϵpII[p,1]
-                if κ <= mpD.cr[p] κ = mpD.cr[p] end
-                f        = J2Yield(ξn,κ)
+                ∂f∂σ = n
+                Δλ   = f/(∂f∂σ'*cmParam.Del*∂f∂σ)
+                Δσ   = (Δλ*cmParam.Del*∂f∂σ)        
+                σ0 .-= Δσ 
+                γ0  += Δλ
+                κ    = max(mpD.cr[p],2.5*mpD.c0[p]+cmParam.Hp*ϵpII[p,1])
+                f    = hasyield(σ[:,p],κ)
                 ηit +=1
             end
             mpD.ϵpII[p,1] = γ0
             σ[:,p]       .= σ0
-            if instr[:fwrk] == :finite
+            if instr[:fwrk] == "finite"
                 # update strain tensor
-                mpD.ϵ[:,:,p].= mutate(cmParam.Del\σ[:,p],0.5,:tensor)
-                # update left cauchy green tensor
-                λ,n          = eigen(mpD.ϵ[:,:,p],sortby=nothing)
-                mpD.b[:,:,p].= n*diagm(exp.(2.0.*λ))*n'
+                mpD.ϵᵢⱼ[:,:,p].= mutate(cmParam.Del\σ[:,p],0.5,:tensor)
+                # update left cauchy green deformation tensor
+                λ,n            = eigen(mpD.ϵᵢⱼ[:,:,p],sortby=nothing)
+                mpD.Bᵢⱼ[:,:,p].= n*diagm(exp.(2.0.*λ))*n'
             end
             ηmax = max(ηit,ηmax)
         end        
