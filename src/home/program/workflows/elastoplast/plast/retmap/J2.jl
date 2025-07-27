f7810196a3319ddc12282362858058d7f8353fac
ewyser <manuwyser@gmail.com>
2025-07-27 17:28:23 +0200

    2nd step

--- src/home/program/workflows/elastoplast/plast/retmap/J2.jl
+++ src/home/program/workflows/elastoplast/plast/retmap/J2.jl
@@ -18,52 +18,60 @@ end
     f    = ξn-κ
     return f,n
 end
-@views @kernel inbounds = true function J2!(mp,cmpr,instr; ftol::Real= 1e-9,ηmax::Int= 20) # Borja (1990); De Souza Neto (2008)
+@views @kernel inbounds = true function finite_J2(mp,cmpr,instr; ftol::Real= 1e-9,ηmax::Int= 20) # Borja (1990); De Souza Neto (2008)
     p = @index(Global)
     if p≤mp.nmp 
-        Hp = cmpr.Hp
-        cᵣ = mp.cᵣ[p]
-        c₀ = mp.c₀[p]
-        # create aliases
-        if instr[:fwrk][:deform] == "finite"
-            σ = mp.τᵢ[:,p]
-        elseif instr[:fwrk][:deform] == "infinitesimal"
-            σ = mp.σᵢ[:,p]
-        end
-        if instr[:nonloc][:status]
-            ϵpII = mp.ϵpII[2,p]
-        else
-            ϵpII = mp.ϵpII[1,p]
-        end
         # calculate yield function
-        κ   = max(cᵣ,c₀+Hp*ϵpII)
-        f,n = yield_J2(σ,κ)
+        κ   = max(mp.cᵣ[p],mp.c₀[p]+cmpr.Hp*mp.ϵpII[2,p])
+        f,n = yield_J2(mp.τᵢ[:,p],κ)
         # return mapping using CPA (non-quadratic convergence)
         if f>0.0 
-            γ0,σ0,ηit = copy(ϵpII),copy(σ),1
+            γ0,σ0,ηit = copy(mp.ϵpII[2,p]),copy(mp.τᵢ[:,p]),1
             while abs(f)>ftol && ηit<ηmax
                 ∂f∂σ = n
                 Δλ   = f/(∂f∂σ'*cmpr.Del*∂f∂σ)
                 Δσ   = (Δλ*cmpr.Del*∂f∂σ)        
                 σ0 .-= Δσ 
                 γ0  += Δλ
-                κ    = max(cᵣ,c₀+Hp*γ0)
+                κ    = max(mp.cᵣ[p],mp.c₀[p]+cmpr.Hp*γ0)
                 f,n  = yield_J2(σ0,κ)
                 ηit +=1
             end
             mp.ϵpII[p,1] = γ0
-            if instr[:fwrk][:deform] == "finite"
-                mp.τᵢ[:,p] .= σ0
-                # update strain tensor
-                mp.ϵᵢⱼ[:,:,p].= mutate(cmpr.Del\σ[:,p],0.5,:tensor)
-                # update left cauchy green deformation tensor
-                λ,n            = eigen(mp.ϵᵢⱼ[:,:,p],sortby=nothing)
-                mp.Bᵢⱼ[:,:,p].= n*diagm(exp.(2.0.*λ))*n'
-            elseif instr[:fwrk][:deform] == "infinitesimal"
-                mp.σᵢ[:,p] .= σ0
-            end   
+            mp.τᵢ[:,p]  .= σ0
+            # update strain tensor
+            mp.ϵᵢⱼ[:,:,p].= mutate(cmpr.Del\mp.τᵢ[:,p],0.5,:tensor)
+            # update left cauchy green deformation tensor
+            λ,n           = eigen(mp.ϵᵢⱼ[:,:,p],sortby=nothing)
+            mp.Bᵢⱼ[:,:,p].= n*diagm(exp.(2.0.*λ))*n'
+            # update plastic corrector increment
             ηmax = max(ηit,ηmax)
-            #==#
+        end        
+    end
+end
+@views @kernel inbounds = true function infinitesimal_J2(mp,cmpr,instr; ftol::Real= 1e-9,ηmax::Int= 20) # Borja (1990); De Souza Neto (2008)
+    p = @index(Global)
+    if p≤mp.nmp 
+        # calculate yield function
+        κ   = max(mp.cᵣ[p],mp.c₀[p]+cmpr.Hp*mp.ϵpII[2,p])
+        f,n = yield_J2(mp.σᵢ[:,p],κ)
+        # return mapping using CPA (non-quadratic convergence)
+        if f>0.0 
+            γ0,σ0,ηit = copy(mp.ϵpII[2,p]),copy(mp.σᵢ[:,p]),1
+            while abs(f)>ftol && ηit<ηmax
+                ∂f∂σ = n
+                Δλ   = f/(∂f∂σ'*cmpr.Del*∂f∂σ)
+                Δσ   = (Δλ*cmpr.Del*∂f∂σ)        
+                σ0 .-= Δσ 
+                γ0  += Δλ
+                κ    = max(mp.cᵣ[p],mp.c₀[p]+cmpr.Hp*γ0)
+                f,n  = yield_J2(σ0,κ)
+                ηit +=1
+            end
+            mp.ϵpII[p,1] = γ0
+            mp.σᵢ[:,p]  .= σ0
+            # update plastic corrector increment
+            ηmax = max(ηit,ηmax)
         end        
     end
 end
